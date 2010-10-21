377a4f40f0c21b8046e0e073ceb8a45c3d9a2dc7
mirkowsk <mirkowsk@localhost>
2010-10-21 14:47:40 +0200

    Cleanup, name changes, fixes

--- src/com/polandro/wifibts/CIDwifiService.java
+++ src/com/polandro/wifibts/CIDwifiService.java
@@ -17,7 +17,6 @@ import android.telephony.gsm.GsmCellLocation;
 import java.sql.Timestamp;
 
 public class CIDwifiService extends Service {
-
 	private TelephonyManager telMgr;
 	private WifiManager wifiMgr;
 	private PhoneStateListener listener;
@@ -26,7 +25,7 @@ public class CIDwifiService extends Service {
 	private int current_cid;
 	private String current_ssid;
 	private SampleReceiver myReceiver;
-	private Timestamp wifiStartTime;
+	private Timestamp serviceStartTime;
 	private int WIFI_DELAY = 10000; //10s
 	
 	private class SampleReceiver extends BroadcastReceiver {
@@ -38,48 +37,42 @@ public class CIDwifiService extends Service {
 	
 	@Override
 	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 		
 	@Override
 	public void onCreate() {
-
 		super.onCreate();
 		startservice();
 	}
 	
 	private void startservice() {
-		OpenCIDdb();
-		wifiStartTime = new Timestamp(0);
-		wifiMgr = (WifiManager)getSystemService(Context.WIFI_SERVICE);
-		telMgr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-		if(wifiMgr == null || telMgr == null){
-			//android fuckup
-			sendMSGtoGUI("Cannot access hardware :(\n");
-			this.onDestroy();
-		}
-		
-		myReceiver = new SampleReceiver();
+		myReceiver = new SampleReceiver();												//Register receiver for messages from GUI
 		IntentFilter filter = new IntentFilter(WifiBTS.NEW_MSG_TO_SERVICE);
         registerReceiver(myReceiver, filter);
 		
-		listener = new PhoneStateListener() {
-            @Override
-            public void onCellLocationChanged(CellLocation location) {            
-            	GCL = (GsmCellLocation)telMgr.getCellLocation();
-            	if(GCL != null){
-            		current_cid = GCL.getCid();
-            		RefreshLACCID();
+		openDB();																		//Open SQLite database
+		serviceStartTime = new Timestamp(0);											//Time when the service was started
+		wifiMgr = (WifiManager)getSystemService(Context.WIFI_SERVICE);					//wifiMgr - connect to the system wifi service
+		telMgr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);			//telMgr - connect to the system telephony service
+		if(wifiMgr == null || telMgr == null) {
+			wifiBTSdb.log(System.currentTimeMillis(), "Cannot access hardware");
+			this.onDestroy();
+		}
+		
+		listener = new PhoneStateListener() {											//Listener for events from telephony manager
+            public void onCellLocationChanged(CellLocation location) {            		//GsmCellLocation changed event
+            	GCL = (GsmCellLocation)telMgr.getCellLocation();						
+            	if (GCL != null) {														//If current location is available
+            		current_cid = GCL.getCid();											//get the actual CellID
+            		refreshCID();													//Refresh status
             	}            	
             }              
         };
-        // Register the listener with the telephony manager
-        telMgr.listen(listener, PhoneStateListener.LISTEN_CELL_LOCATION);
+        telMgr.listen(listener, PhoneStateListener.LISTEN_CELL_LOCATION);				//Register the listener with the telephony manager
 	}
 	
-	private void showDataFromIntent(Intent intent) { //request parser
-        	
+	private void showDataFromIntent(Intent intent) { //request parser        	
 			int order = intent.getIntExtra("ToService", -1);
 			if(order == WifiBTS.PING){
 				sendMSGtoGUI("Service already running.\n"); 
@@ -89,8 +82,8 @@ public class CIDwifiService extends Service {
 			}
     }
 	
-	private void startWifiManually(){
-		wifiStartTime.setTime(System.currentTimeMillis());
+	private void startWifiManually() {
+		serviceStartTime.setTime(System.currentTimeMillis());
 		wifiMgr.setWifiEnabled(true);
 		wifiBTSdb.log(System.currentTimeMillis(), "wifi enabled manually");
 	}
@@ -101,7 +94,7 @@ public class CIDwifiService extends Service {
     	sendBroadcast(intent);
 	}
 	
-	private void OpenCIDdb() {
+	private void openDB() {
 		try {
 			wifiBTSdb = new DBAdapter(this);
 			wifiBTSdb.open();
@@ -111,7 +104,7 @@ public class CIDwifiService extends Service {
 		}
     }
 	
-	private void RefreshLACCID() {
+	private void refreshCID() {
 		try{
 	    	WifiInfo winfo = wifiMgr.getConnectionInfo();
 	    	current_ssid = winfo.getSSID();
@@ -125,9 +118,8 @@ public class CIDwifiService extends Service {
 	        }
 	    	
 	        Timestamp current_time = new Timestamp(System.currentTimeMillis());
-	    	//<LOGIC>
 	    	if(current_cid != -1 &&
-	    			current_time.getTime() - wifiStartTime.getTime() > WIFI_DELAY){
+	    			current_time.getTime() - serviceStartTime.getTime() > WIFI_DELAY){
 		    	if(!wifiMgr.isWifiEnabled()){
 		    		if(wifiBTSdb.checkCID(current_cid)){
 		    			wifiMgr.setWifiEnabled(true);
@@ -146,7 +138,6 @@ public class CIDwifiService extends Service {
 		    		}    		
 		    	}
 	    	}
-	    	//</LOGIC>
 		}
 		catch(Exception e){
 			sendMSGtoGUI(e.getMessage());
@@ -156,10 +147,10 @@ public class CIDwifiService extends Service {
 	
 	@Override
 	public void onDestroy() {
-		try{
+		try {
 			wifiBTSdb.close();
 		}
-		catch(SQLiteException e){
+		catch (SQLiteException e) {
 			sendMSGtoGUI("DB exception!!!"); 
 		}
 		sendMSGtoGUI("Service stopped.\n"); 
